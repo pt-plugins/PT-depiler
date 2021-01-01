b4e7b996c5c50e0c507b0869b3d19b7ad6f05d63
Rhilip <rhilipruan@gmail.com>
2021-01-01 23:02:10 +0800

    pref(BtClient): 增加已有7种Bt客户端的icon(128x128)

---
+++ src/background/sites/schema/base.ts
@@ -0,0 +1,30 @@
+
+interface Torrent {
+
+}
+
+interface searchFilter {
+  keywords: string
+}
+
+interface searchResult {
+  torrent: Torrent[]
+}
+
+type SiteFeature = ''
+
+// 适用于公网BT站点
+export abstract class BittorrentSite {
+  protected readonly feature : {
+    [key in SiteFeature]: boolean
+  } = {
+    '': false
+  }
+
+  abstract searchTorrents(filter: searchFilter) : Promise<searchResult>;
+}
+
+// 适用于PT站点
+export abstract class PrivateSite extends BittorrentSite {
+
+}
