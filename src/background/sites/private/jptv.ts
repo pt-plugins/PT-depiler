98d50c8f7a849688333b669dc16a9218d22ddf44
Rhilip <rhilipruan@gmail.com>
2021-05-20 09:56:25 +0800

    feat(Site): 跟进上游pr添加2个站点
    refs: #802 #792

---
+++ src/background/sites/private/jptv.ts
@@ -0,0 +1,11 @@
+import { SiteMetadata } from '@/shared/interfaces/sites';
+
+export const siteMetadata: SiteMetadata = {
+  name: 'JPTV',
+  timezoneOffset: '+0800',
+  schema: 'Unit3D',
+  url: 'https://jptv.club/',
+  description: 'JPTV',
+  tags: ['影视', '剧集', '动漫'],
+  collaborator: 'MewX'
+};
---
+++ src/background/sites/private/ultrahd.ts
@@ -0,0 +1,51 @@
+import { SiteMetadata } from '@/shared/interfaces/sites';
+import { ETorrentStatus } from '@/shared/interfaces/enum';
+
+export const siteMetadata: SiteMetadata = {
+  name: 'UltraHD',
+  timezoneOffset: '+0800',
+  schema: 'NexusPHP',
+  url: 'https://ultrahd.net/',
+  description: '韩剧',
+  tags: ['电影', '电视剧', '综艺', '纪录片', '动漫'],
+  search: {
+    categories: [
+      {
+        name: '类别',
+        key: 'cat',
+        options: [
+          { name: '电影', value: 401 },
+          { name: '电视剧', value: 402 },
+          { name: '综艺', value: 403 },
+          { name: '纪录片', value: 404 },
+          { name: '动漫', value: 405 }
+        ],
+        cross: { mode: 'append' }
+      }
+    ]
+  },
+
+  selector: {
+    search: {
+      progress: {
+        selector: '.torrentname td:first-child > div[title]:last-child',
+        attr: 'title',
+        filters: [
+          (query: string) => {
+            const queryMatch = query.match(/([.\d]+)%/);
+            return queryMatch && queryMatch.length >= 2 ? parseFloat(queryMatch[1]) : 0;
+          }
+        ]
+      },
+      status: {
+        selector: '.torrentname td:first-child > div[title]:last-child',
+        case: {
+          '[title^="leeching"]': ETorrentStatus.downloading,
+          '[title^="seeding"]': ETorrentStatus.seeding,
+          '[title^="inactivity"]': ETorrentStatus.completed
+        }
+      }
+    }
+  }
+
+};
