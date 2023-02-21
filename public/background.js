0ca45604872c46c767cac01cb1174f85c08abdca
Rhilip <admin@rhilip.info>
2023-02-21 22:36:49 +0800

    fix(offscreen): fix offscreen create error
    Uncaught (in promise) Error: Only a single `reason` is currently supported.
    f**k google, you say this is `Reason[]` in [document](https://developer.chrome.com/docs/extensions/reference/offscreen/#type-CreateParameters)

--- public/background.js
+++ public/background.js
@@ -5,7 +5,7 @@ var window = self;
 // create offscreen for DOM_PARSER and other reason ( f**k google )
 chrome.offscreen.createDocument({
   url: "offscreen.html",
-  reasons: [chrome.offscreen.Reason.DOM_PARSER, chrome.offscreen.Reason.CLIPBOARD, chrome.offscreen.Reason.BLOBS],
+  reasons: [chrome.offscreen.Reason.DOM_PARSER],
   justification: "Allow DOM_PARSER, CLIPBOARD, BLOBS in background.",
 });
 
