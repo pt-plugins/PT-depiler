1ef21f0583ecdfae574fbd6c28e30eca282e5977
Rhilip <admin@rhilip.info>
2025-04-14 19:03:51 +0800

    feat(AdvanceFilterGenerateDialog): 允许快捷设置日期为 本日等时间段 及 按日历选择
    closed: https://github.com/pt-plugins/PT-Plugin-Plus/issues/2146

--- src/entries/options/views/Overview/SearchEntity/AdvanceFilterGenerateDialog.vue
+++ src/entries/options/views/Overview/SearchEntity/AdvanceFilterGenerateDialog.vue
@@ -5,11 +5,12 @@
  * 如果需要忽略，目前只能重置过滤词。
  * refs: https://github.com/vuetifyjs/vuetify/blob/0ca7e93ad011b358591da646fdbd6ebe83625d25/packages/vuetify/src/components/VCheckbox/VCheckboxBtn.tsx#L49-L53
  */
-import { format } from "date-fns";
 import { filesize } from "filesize";
-import { computed, reactive, watch } from "vue";
+import { useI18n } from "vue-i18n";
+import { computed, reactive, ref, watch } from "vue";
 import { uniq, flatten, uniqBy } from "es-toolkit";
 import searchQueryParser from "search-query-parser";
+import { format, startOfDay, startOfWeek, startOfMonth, startOfQuarter, startOfYear } from "date-fns";
 
 import { useRuntimeStore } from "@/options/stores/runtime.ts";
 import { searchQueryParserOptions } from "./utils.ts";
@@ -17,10 +18,11 @@ import { searchQueryParserOptions } from "./utils.ts";
 import SiteName from "@/options/components/SiteName.vue";
 import SiteFavicon from "@/options/components/SiteFavicon.vue";
 
-const runtimeStore = useRuntimeStore();
-
 const showDialog = defineModel<boolean>();
 const emit = defineEmits(["update:tableFilter", "update:modelValue"]);
+
+const { t } = useI18n();
+const runtimeStore = useRuntimeStore();
 
 interface ITextValue {
   required: string[];
@@ -33,6 +35,7 @@ interface IKeywordValue<T> extends ITextValue {
 
 interface IRangedValue {
   range: [number, number];
+  ticks: number[];
   value: [number, number];
 }
 
@@ -58,11 +61,11 @@ const advanceFilterDict = reactive<IAdvanceFilterDict>({
   text: { required: [], exclude: [] },
   site: { all: [], required: [], exclude: [] },
   tags: { all: [], required: [], exclude: [] },
-  date: { range: [0, 0], value: [0, 0] },
-  size: { range: [0, 0], value: [0, 0] },
-  seeders: { range: [0, 0], value: [0, 0] },
-  leechers: { range: [0, 0], value: [0, 0] },
-  completed: { range: [0, 0], value: [0, 0] },
+  date: { range: [0, 0], ticks: [], value: [0, 0] },
+  size: { range: [0, 0], ticks: [], value: [0, 0] },
+  seeders: { range: [0, 0], ticks: [], value: [0, 0] },
+  leechers: { range: [0, 0], ticks: [], value: [0, 0] },
+  completed: { range: [0, 0], ticks: [], value: [0, 0] },
 });
 
 function generateRange(data: (number | undefined)[]): [number, number] {
@@ -70,12 +73,40 @@ function generateRange(data: (number | undefined)[]): [number, number] {
   return [Math.min(...numData), Math.max(...numData)];
 }
 
-function generateRangeField(data: (number | undefined)[]): IRangedValue {
-  const range = generateRange(data);
-  return { range, value: range };
+function getThisDateUnitRange(dateType: "day" | "week" | "month" | "quarter" | "year"): [number, number] {
+  const [minDate, maxDate] = advanceFilterDict.date.range;
+
+  const now = new Date();
+  const start = {
+    day: startOfDay(now),
+    week: startOfWeek(now),
+    month: startOfMonth(now),
+    quarter: startOfQuarter(now),
+    year: startOfYear(now),
+  }[dateType];
+
+  return [Math.max(minDate, start.getTime()), Math.min(maxDate, now.getTime())];
 }
 
+function isThisDateInRange(value: unknown): boolean {
+  const valueTime = (value as Date).getTime();
+  const [minDate, maxDate] = advanceFilterDict.date.range;
+  return valueTime >= minDate && valueTime <= maxDate;
+}
+
+function generateRangeField(data: (number | undefined)[]): IRangedValue {
+  const range = generateRange(data);
+  return { range, ticks: Array.from(new Set(data)) as number[], value: range };
+}
+
+function setDateRangeByDatePicker(value: unknown[]) {
+  const dateRange = value as Date[];
+  advanceFilterDict.date.value = [dateRange[0].getTime(), dateRange[dateRange.length - 1].getTime()];
+}
+
+const resetCount = ref<number>(0);
 function resetFilter() {
+  resetCount.value = +new Date(); // 更新重置计数，触发 vue 更新 site 和 tags 的 v-checkbox ，防止因为 :key 的问题导致无法重置
   const searchResult = runtimeStore.search.searchResult;
 
   advanceFilterDict.text = { required: [], exclude: [] };
@@ -180,7 +211,7 @@ function updateTableFilter() {
           </v-row>
           <v-row><v-label>站点</v-label></v-row>
           <v-row>
-            <v-col v-for="site in advanceFilterDict.site.all" :key="site" class="pa-0" :cols="3">
+            <v-col v-for="site in advanceFilterDict.site.all" :key="`${resetCount}_${site}`" class="pa-0" :cols="3">
               <v-checkbox
                 v-model="advanceFilterDict.site.required"
                 :label="site"
@@ -199,7 +230,7 @@ function updateTableFilter() {
           </v-row>
           <v-row><v-label>标签</v-label></v-row>
           <v-row>
-            <v-col v-for="tag in advanceFilterDict.tags.all" :key="tag.name" :cols="2" class="pa-0">
+            <v-col v-for="tag in advanceFilterDict.tags.all" :key="`${resetCount}_${tag.name}`" :cols="2" class="pa-0">
               <v-checkbox
                 v-model="advanceFilterDict.tags.required"
                 :value="tag.name"
@@ -218,7 +249,33 @@ function updateTableFilter() {
           </v-row>
           <v-row>
             <v-col cols="6">
-              <v-row><v-label>日期</v-label></v-row>
+              <v-row class="pr-4">
+                <v-label>日期</v-label>
+                <v-spacer />
+                <v-chip
+                  v-for="dateUnit in ['day', 'week', 'month', 'quarter', 'year'] as const"
+                  :key="dateUnit"
+                  size="x-small"
+                  class="mr-1"
+                  @click="() => (advanceFilterDict.date.value = getThisDateUnitRange(dateUnit))"
+                >
+                  {{ t(`SearchEntity.AdvanceFilterGenerateDialog.date.${dateUnit}`) }}
+                </v-chip>
+                <v-chip size="x-small">
+                  {{ t("SearchEntity.AdvanceFilterGenerateDialog.date.custom") }}
+                  <v-menu activator="parent" location="top" :close-on-content-click="false">
+                    <v-date-picker
+                      :allowed-dates="isThisDateInRange"
+                      :max="new Date(advanceFilterDict.date.range[1])"
+                      :min="new Date(advanceFilterDict.date.range[0])"
+                      hide-header
+                      multiple="range"
+                      show-adjacent-months
+                      @update:model-value="(v) => setDateRangeByDatePicker(v)"
+                    ></v-date-picker>
+                  </v-menu>
+                </v-chip>
+              </v-row>
               <v-row>
                 <v-range-slider
                   v-model="advanceFilterDict.date.value"
@@ -226,9 +283,13 @@ function updateTableFilter() {
                   :min="advanceFilterDict.date.range[0]"
                   :step="60 * 1000"
                   :thumb-label="true"
+                  :ticks="advanceFilterDict.date.ticks"
                   class="px-6"
                   hide-details
+                  show-ticks="always"
+                  tick-size="4"
                 >
+                  <template #tick-label></template>
                   <template #thumb-label="{ modelValue }">
                     <span class="text-no-wrap">{{ format(modelValue ?? 0, "yyyy-MM-dd HH:mm") }}</span>
                   </template>
@@ -244,9 +305,13 @@ function updateTableFilter() {
                   :min="advanceFilterDict.size.range[0]"
                   :step="1024 ** 3"
                   :thumb-label="true"
+                  :ticks="advanceFilterDict.size.ticks"
                   class="px-6"
                   hide-details
+                  show-ticks="always"
+                  tick-size="4"
                 >
+                  <template #tick-label></template>
                   <template #thumb-label="{ modelValue }">
                     <span class="text-no-wrap">{{ filesize(modelValue ?? 0) }}</span>
                   </template>
@@ -263,10 +328,15 @@ function updateTableFilter() {
                   :max="advanceFilterDict.seeders.range[1]"
                   :min="advanceFilterDict.seeders.range[0]"
                   :thumb-label="true"
+                  :ticks="advanceFilterDict.seeders.ticks"
                   class="px-6"
                   hide-details
+                  show-ticks="always"
                   step="1"
-                />
+                  tick-size="4"
+                >
+                  <template #tick-label></template>
+                </v-range-slider>
               </v-row>
             </v-col>
             <v-col cols="4">
@@ -277,10 +347,15 @@ function updateTableFilter() {
                   :max="advanceFilterDict.leechers.range[1]"
                   :min="advanceFilterDict.leechers.range[0]"
                   :thumb-label="true"
+                  :ticks="advanceFilterDict.leechers.ticks"
                   class="px-6"
                   hide-details
+                  show-ticks="always"
                   step="1"
-                />
+                  tick-size="4"
+                >
+                  <template #tick-label></template>
+                </v-range-slider>
               </v-row>
             </v-col>
             <v-col cols="4">
@@ -291,10 +366,15 @@ function updateTableFilter() {
                   :max="advanceFilterDict.completed.range[1]"
                   :min="advanceFilterDict.completed.range[0]"
                   :thumb-label="true"
+                  :ticks="advanceFilterDict.completed.ticks"
                   class="px-6"
                   hide-details
+                  show-ticks="always"
                   step="1"
-                />
+                  tick-size="4"
+                >
+                  <template #tick-label></template>
+                </v-range-slider>
               </v-row>
             </v-col>
           </v-row>
