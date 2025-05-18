7b24732bdcea91e082a6b576f81eea1f1035ab82
ex-hentai <96158157+ex-hentai@users.noreply.github.com>
2025-05-18 10:33:15 +0800

    build: chrome 和 firefox 使用不同输出目录 (#28)

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,11 @@ const permissions = [
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "es2023",
+    outDir: `dist-${target}`,
+    emptyOutDir: true,
+  },
   css: {
     preprocessorOptions: {
       scss: {
