9b0d9357dbc904c09665956fec4d96572e6555ad
hui-shao <hui-shao@outlook.com>
2025-05-29 13:06:59 +0800

    feat(pkg/site): 站点适配 PTLGS

---
+++ src/packages/site/definitions/ptlgs.ts
@@ -0,0 +1,228 @@
+import { ISiteMetadata } from "@ptd/site";
+import { CategoryInclbookmarked, CategoryIncldead, CategorySpstate, SchemaMetadata } from "../schemas/NexusPHP";
+
+export const siteMetadata: ISiteMetadata = {
+  ...SchemaMetadata,
+  version: 1,
+
+  id: "ptlgs",
+  name: "PTLGS",
+  aka: ["劳改所"],
+  description: "PTLGS(劳改所)，黑奴的最终归宿",
+  tags: ["综合", "影视"],
+  timezoneOffset: "+0800",
+
+  type: "private",
+  schema: "NexusPHP",
+
+  collaborator: ["yxlimo", "hui-shao"],
+
+  urls: ["aHR0cHM6Ly9wdGxncy5vcmcv"],
+
+  category: [
+    {
+      name: "分类",
+      key: "cat",
+      options: [
+        { value: 401, name: "Movies(电影)" },
+        { value: 402, name: "TV Series(剧集)" },
+        { value: 403, name: "TV Shows(综艺)" },
+        { value: 404, name: "Docs(纪录)" },
+        { value: 405, name: "Animations(动画)" },
+        { value: 406, name: "Music(音乐)" },
+        { value: 407, name: "Sports(体育)" },
+        { value: 409, name: "Others(其他)" },
+      ],
+      cross: { mode: "append" },
+    },
+    {
+      name: "媒介",
+      key: "medium",
+      options: [
+        { value: 14, name: "Blu-ray" },
+        { value: 8, name: "Remux" },
+        { value: 5, name: "BDRip" },
+        { value: 4, name: "WEB-DL" },
+        { value: 7, name: "WEBRip" },
+        { value: 3, name: "HDTV" },
+        { value: 2, name: "TVRip" },
+        { value: 1, name: "DVD" },
+        { value: 11, name: "DVDRip" },
+        { value: 15, name: "CD" },
+        { value: 12, name: "Other" },
+      ],
+      cross: { mode: "append" },
+    },
+    {
+      name: "分辨率",
+      key: "standard",
+      options: [
+        { value: 6, name: "2160p" },
+        { value: 1, name: "1080p" },
+        { value: 2, name: "1080i" },
+        { value: 3, name: "720p" },
+        { value: 4, name: "SD" },
+        { value: 7, name: "Other" },
+      ],
+      cross: { mode: "append" },
+    },
+    {
+      name: "标签",
+      key: "tag_id",
+      options: [
+        { value: 22, name: "独家" },
+        { value: 17, name: "驻站" },
+        { value: 1, name: "禁转" },
+        { value: 2, name: "首发" },
+        { value: 3, name: "官方" },
+        { value: 4, name: "DIY" },
+        { value: 6, name: "中字" },
+        { value: 20, name: "3D" },
+        { value: 5, name: "国语" },
+        { value: 9, name: "原盘" },
+        { value: 11, name: "REMUX" },
+        { value: 14, name: "WEB-DL" },
+        { value: 8, name: "DoVi" },
+        { value: 7, name: "HDR" },
+        { value: 12, name: "SDR" },
+        { value: 10, name: "特效字幕" },
+      ],
+      cross: false,
+    },
+    CategoryIncldead,
+    CategorySpstate,
+    CategoryInclbookmarked,
+  ],
+
+  userInfo: {
+    ...SchemaMetadata.userInfo!,
+    selectors: {
+      ...SchemaMetadata.userInfo!.selectors!,
+      bonus: {
+        selector: ["td.rowhead:contains('工分') + td", "td.rowhead:contains('Karma'):contains('Points') + td"],
+        filters: [{ name: "parseNumber" }],
+      },
+    },
+  },
+
+  levelRequirements: [
+    {
+      id: 1,
+      name: "Peasant",
+      privilege: "被降级的用户",
+    },
+    {
+      id: 2,
+      name: "User",
+      privilege: "新用户的默认级别。",
+    },
+    {
+      id: 3,
+      name: "Power User",
+      downloaded: "100GB",
+      ratio: 1.1,
+      seedingPoints: 20000,
+      privilege:
+        '可以直接发布种子；可以查看NFO文档；可以查看用户列表；可以请求续种； 可以发送邀请； 可以查看排行榜；可以查看其它用户的种子历史(如果用户隐私等级未设置为"强")；可以删除自己上传的字幕。',
+    },
+    {
+      id: 4,
+      name: "Elite User",
+      downloaded: "200GB",
+      ratio: 1.2,
+      seedingPoints: 50000,
+      privilege: "",
+    },
+    {
+      id: 5,
+      name: "Crazy User",
+      downloaded: "500GB",
+      ratio: 1.2,
+      seedingPoints: 100000,
+      privilege: "得到两个邀请名额；可以在做种/下载/发布的时候选择匿名模式。",
+    },
+    {
+      id: 6,
+      name: "Insane User",
+      downloaded: "1TB",
+      ratio: 1.2,
+      seedingPoints: 200000,
+      privilege: "可以查看普通日志。",
+    },
+    {
+      id: 7,
+      name: "Veteran User",
+      downloaded: "2TB",
+      ratio: 1.2,
+      seedingPoints: 500000,
+      privilege: "得到一个邀请名额；可以查看其它用户的评论、帖子历史。Veteran User及以上用户会永远保留账号。",
+    },
+    {
+      id: 8,
+      name: "Extreme User",
+      downloaded: "3TB",
+      ratio: 1.5,
+      seedingPoints: 800000,
+      privilege: "可以更新过期的外部信息；可以查看Extreme User论坛。",
+    },
+    {
+      id: 9,
+      name: "Ultimate User",
+      downloaded: "4TB",
+      ratio: 1.5,
+      seedingPoints: 1000000,
+      privilege: "得到一个邀请名额。",
+    },
+    {
+      id: 10,
+      name: "Nexus Master",
+      downloaded: "5TB",
+      ratio: 1.5,
+      seedingPoints: 2500000,
+      privilege: "得到一个邀请名额。",
+    },
+    {
+      id: 100,
+      groupType: "vip",
+      name: "贵宾(VIP)",
+      privilege: "",
+    },
+    {
+      id: 200,
+      groupType: "manager",
+      name: "养老族(Retiree)",
+      privilege: "退休后的管理组成员。",
+    },
+    {
+      id: 201,
+      groupType: "manager",
+      name: "发布员(Uploader)",
+      privilege: "专注的发布者。免除自动降级；可以查看匿名用户的真实身份。",
+    },
+    {
+      id: 202,
+      groupType: "manager",
+      name: "总版主(Moderator)",
+      privilege:
+        "可以查看管理组信箱、举报信箱；管理趣味盒内容、投票内容；可以编辑或删除任何发布的种子；可以管理候选；可以管理论坛帖子、用户评论；可以查看机密日志；可以删除任何字幕；可以管理日志中的代码、史册；可以查看用户的邀请记录；可以管理用户帐号的一般信息。不能管理友情链接、最近消息、论坛版块；不能将种子设为置顶或促销；不能查看用户IP或Email等机密信息；不能删除账号。",
+    },
+    {
+      id: 203,
+      groupType: "manager",
+      name: "管理员(Administrator)",
+      privilege: "除了不能改变站点设定、管理捐赠外，可以做任何事。",
+    },
+    {
+      id: 204,
+      groupType: "manager",
+      name: "维护开发员(Sysop)",
+      privilege: "网站开发/维护人员，可以改变站点设定，不能管理捐赠。",
+    },
+    {
+      id: 205,
+      groupType: "manager",
+      name: "主管(Staff Leader)",
+      privilege: "网站主管，可以做任何事。",
+    },
+  ],
+};
