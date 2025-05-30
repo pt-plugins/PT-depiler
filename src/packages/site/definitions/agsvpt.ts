3f412c995d00e12c1698e27c020ff84e9d3e7d2f
null <wzy199498@foxmail.com>
2025-05-30 17:18:54 +0800

    feat(pkg/site): 站点适配 AGSV

---
+++ src/packages/site/definitions/agsvpt.ts
@@ -0,0 +1,97 @@
+import { ISiteMetadata } from "@ptd/site";
+import { SchemaMetadata } from "../schemas/NexusPHP";
+
+export const siteMetadata: ISiteMetadata = {
+  ...SchemaMetadata,
+  version: 1,
+  id: "agsvpt",
+  name: "AGSVPT",
+  aka: ["末日种子库"],
+  description: "Arctic Global Seed Vault", // 站点说明
+  tags: ["综合", "短剧", "影视"],
+  timezoneOffset: "+0800",
+  collaborator: ["0X0000005"],
+  type: "private",
+  schema: "NexusPHP",
+  urls: ["aHR0cHM6Ly9wdC5hZ3N2cHQuY24=", "aHR0cHM6Ly93d3cuYWdzdnB0LmNvbQ==", "aHR0cHM6Ly9uZXcuYWdzdnB0LmNu"],
+  userInfo: {
+    ...SchemaMetadata.userInfo!,
+    selectors: {
+      ...SchemaMetadata.userInfo!.selectors!,
+      bonus: {
+        selector: ["td.rowhead:contains('冰晶') + td, td.rowhead:contains('Karma Points') + td"],
+        filters: [{ name: "parseNumber" }],
+      },
+    },
+  },
+  levelRequirements: [
+    {
+      id: 1,
+      name: "Power User(北冰珍珠熊)",
+      downloaded: "50GB",
+      ratio: 1.05,
+      seedingBonus: 40000,
+      privilege: "可以进入银行贷款",
+    },
+    {
+      id: 2,
+      name: "Elite User(深渊蔚蓝熊)",
+      interval: "P8W",
+      downloaded: "120GB",
+      ratio: 1.55,
+      seedingBonus: 80000,
+    },
+    {
+      id: 3,
+      name: "Crazy User(翡翠森林熊)",
+      interval: "P12W",
+      downloaded: "300GB",
+      ratio: "2.05",
+      seedingBonus: 150000,
+    },
+    {
+      id: 4,
+      name: "Insane User(神秘紫晶熊)",
+      interval: "P20W",
+      downloaded: "500GB",
+      ratio: 2.55,
+      seedingBonus: 400000,
+      privilege: "查看普通日志",
+    },
+    {
+      id: 5,
+      name: "Veteran User(寒冰白金熊)",
+      interval: "P28W",
+      downloaded: "750GB",
+      ratio: 4.05,
+      seedingBonus: 800000,
+      privilege: "永远保留账号；查看其它用户的评论、帖子历史",
+    },
+    {
+      id: 6,
+      name: "Extreme User(皇家金辉熊)",
+      interval: "P40W",
+      downloaded: "1TB",
+      ratio: 5.05,
+      seedingBonus: 1400000,
+    },
+    {
+      id: 7,
+      name: "Ultimate User(永恒铂金熊)",
+      interval: "P52W",
+      downloaded: "1.5TB",
+      ratio: "6.05",
+      seedingBonus: 2200000,
+      privilege: "首次升级至此等级的用户将获得2个邀请名额。",
+    },
+    {
+      id: 8,
+      name: "Nexus Master(钻石之冠北极熊)",
+      interval: "P70W",
+      downloaded: "3TB",
+      ratio: 7.05,
+      seedingBonus: 3200000,
+      privilege: "首次升级至此等级的用户将获得2个邀请名额。",
+    },
+  ],
+};
