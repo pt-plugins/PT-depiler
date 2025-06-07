0f048d9c20d300be52c1e0903b1d40055b927005
yanlc <yanleichang@vip.163.com>
2025-06-07 15:42:23 +0800

    feat(pkg/site): 适配站点 sunny

---
+++ src/packages/site/definitions/sunnypt.ts
@@ -0,0 +1,185 @@
+import { type ISiteMetadata } from "../types";
+import { CategoryInclbookmarked, CategoryIncldead, CategorySpstate, SchemaMetadata } from "../schemas/NexusPHP.ts";
+
+export const siteMetadata: ISiteMetadata = {
+  ...SchemaMetadata,
+
+  version: 1,
+  id: "sunnypt",
+  name: "Sunny",
+  aka: ["SunnyPT"],
+  description: "The Ultimate File Sharing Experience",
+  tags: ["影视", "综合"],
+  timezoneOffset: "+0800",
+  favicon: "./sunnypt.ico",
+
+  collaborator: ["yanleichang"],
+
+  type: "private",
+  schema: "NexusPHP",
+
+  urls: ["ROT13:uggcf://fhaalcg.gbc/"],
+
+  category: [
+    {
+      name: "分类",
+      key: "cat",
+      options: [
+        { name: "电影", value: 401 },
+        { name: "电视剧", value: 402 },
+        { name: "综艺", value: 403 },
+        { name: "动漫", value: 404 },
+        { name: "纪录片", value: 405 },
+        { name: "体育", value: 406 },
+        { name: "MV", value: 407 },
+        { name: "音轨", value: 408 },
+        { name: "其他", value: 409 },
+      ],
+      cross: { mode: "append" },
+    },
+    {
+      name: "视频类",
+      key: "medium",
+      options: [
+        { name: "UHD Blu-ray", value: 11 },
+        { name: "Blu-ray", value: 1 },
+        { name: "Remux", value: 3 },
+        { name: "WEB-DL", value: 10 },
+        { name: "Encode", value: 6 },
+        { name: "HDTV", value: 5 },
+        { name: "MiniBD", value: 4 },
+        { name: "HD DVD", value: 7 },
+        { name: "DVD", value: 8 },
+        { name: "Other", value: 12 },
+      ],
+      cross: { mode: "append" },
+    },
+    {
+      name: "分辨率",
+      key: "standard",
+      options: [
+        { name: "4320P/8K/FUHD", value: 7 },
+        { name: "2160P/4K/UHD", value: 8 },
+        { name: "1080p/1080i/FHD", value: 1 },
+        { name: "720p/720i/HD", value: 6 },
+        { name: "360p/360i/SD", value: 5 },
+        { name: "Other", value: 9 },
+      ],
+      cross: { mode: "append" },
+    },
+    {
+      name: "音频类",
+      key: "audiocodec",
+      options: [
+        { name: "2160p", value: 5 },
+        { name: "1080p", value: 1 },
+        { name: "1080i", value: 2 },
+        { name: "720p", value: 3 },
+        { name: "SD", value: 4 },
+        { name: "Other", value: 6 },
+      ],
+      cross: { mode: "append" },
+    },
+    {
+      name: "制作组",
+      key: "team",
+      options: [
+        { name: "Sunny", value: 6 },
+        { name: "Other", value: 5 },
+      ],
+      cross: { mode: "append" },
+    },
+    CategoryIncldead,
+    CategorySpstate,
+    CategoryInclbookmarked,
+  ],
+
+  userInfo: {
+    ...SchemaMetadata.userInfo!,
+    selectors: {
+      ...SchemaMetadata.userInfo!.selectors!,
+    },
+  },
+
+  levelRequirements: [
+    {
+      id: 1,
+      name: "User",
+      enName: "User",
+      privilege: "新用户的默认级别。只能在每周六中午12点至每周日晚上11点59分发布种子。",
+    },
+    {
+      id: 2,
+      name: "Power User",
+      interval: "P4W",
+      downloaded: "50GB",
+      ratio: 1.05,
+      seedingBonus: 40000,
+      privilege:
+        '得到一个邀请名额；可以直接发布种子；可以查看NFO文档；可以查看用户列表；可以请求续种； 可以发送邀请； 可以查看排行榜；可以查看其它用户的种子历史(如果用户隐私等级未设置为"强")； 可以删除自己上传的字幕。',
+    },
+    {
+      id: 3,
+      name: "Elite User",
+      interval: "P8W",
+      downloaded: "120GB",
+      ratio: 1.55,
+      seedingBonus: 80000,
+      privilege: "Elite User及以上用户封存账号后不会被删除。",
+    },
+    {
+      id: 4,
+      name: "Crazy User",
+      interval: "P15W",
+      downloaded: "300GB",
+      ratio: 2.05,
+      seedingBonus: 150000,
+      privilege: "得到两个邀请名额；可以在做种/下载/发布的时候选择匿名模式。",
+    },
+    {
+      id: 5,
+      name: "Insane User",
+      interval: "P25W",
+      downloaded: "500GB",
+      ratio: 2.55,
+      seedingBonus: 250000,
+      privilege: "可以查看普通日志。",
+    },
+    {
+      id: 6,
+      name: "Veteran User",
+      interval: "P40W",
+      downloaded: "750GB",
+      ratio: 3.05,
+      seedingBonus: 400000,
+      privilege: "得到三个邀请名额；可以查看其它用户的评论、帖子历史。Veteran User及以上用户会永远保留账号。",
+    },
+    {
+      id: 7,
+      name: "Extreme User",
+      interval: "P60W",
+      downloaded: "1TB",
+      ratio: 3.55,
+      seedingBonus: 600000,
+      privilege: "可以更新过期的外部信息；可以查看Extreme User论坛。",
+    },
+    {
+      id: 8,
+      name: "Ultimate User",
+      interval: "P80W",
+      downloaded: "1.5TB",
+      ratio: 4.05,
+      seedingBonus: 800000,
+      privilege: "得到五个邀请名额。",
+    },
+    {
+      id: 9,
+      name: "Nexus Master",
+      interval: "P100W",
+      downloaded: "3TB",
+      ratio: 4.55,
+      seedingBonus: 1000000,
+      privilege: "得到十个邀请名额。",
+    },
+  ],
+};
