f00dc96332fd595d239353b1584e7af439983398
Rhilip <admin@rhilip.info>
2025-06-11 21:43:15 +0800

    feat(pkg/site): 增加 DEAD 站点 npupt 和 ydypt
    refs: https://github.com/pt-plugins/PT-depiler/issues/143
    refs: https://github.com/pt-plugins/PT-depiler/discussions/137

---
+++ src/packages/site/definitions/ydypt.ts
@@ -0,0 +1,17 @@
+import type { ISiteMetadata } from "../types";
+
+export const siteMetadata: ISiteMetadata = {
+  version: 1,
+  id: "ydypt",
+  name: "伊甸园",
+  description: "这里是伊甸园 让我们赤裸坦诚相见",
+
+  favicon: "./ydypt.jpg",
+
+  type: "private",
+  schema: "NexusPHP",
+
+  urls: ["https://pt.hdbd.us/"],
+
+  isDead: true,
+};
