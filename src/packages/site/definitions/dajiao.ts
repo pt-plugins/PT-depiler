ece1d27c8ace936269591000a4f02914bd3510e4
hui-shao <hui-shao@outlook.com>
2025-06-11 16:33:50 +0800

    feat(pkg/site): 添加站点 ECUSTPT 和 DaJiao
    Dead.

---
+++ src/packages/site/definitions/dajiao.ts
@@ -0,0 +1,16 @@
+import type { ISiteMetadata } from "../types";
+
+export const siteMetadata: ISiteMetadata = {
+  version: 1,
+  id: "dajiao",
+  name: "DAJIAO",
+  description: "打胶",
+  tags: ["综合", "纪录片"],
+
+  type: "private",
+  schema: "NexusPHP",
+
+  urls: ["https://dajiao.cyou/"],
+
+  isDead: true,
+};
---
+++ src/packages/site/definitions/ecustpt.ts
@@ -0,0 +1,17 @@
+import type { ISiteMetadata } from "../types";
+
+export const siteMetadata: ISiteMetadata = {
+  version: 1,
+  id: "ecustpt",
+  name: "ECUSTPT",
+  description: "华东理工大学PT站，逢考必过",
+  tags: ["教育网", "影视", "综合"],
+
+  type: "private",
+  schema: "NexusPHP",
+
+  urls: ["https://pt.ecust.pp.ua/"],
+  formerHosts: ["public.ecustpt.eu.org", "ecustpt.eu.org"],
+
+  isDead: true,
+};
