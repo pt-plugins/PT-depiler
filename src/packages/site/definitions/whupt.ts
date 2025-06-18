06cae65d1a34c0529a8651a0471a05d08269503f
Rhilip <admin@rhilip.info>
2025-06-18 21:09:28 +0800

    feat(pkg/site): 增加 dead 站点 tmpt, whupt
    refs: https://github.com/pt-plugins/PT-depiler/issues/143

---
+++ src/packages/site/definitions/whupt.ts
@@ -0,0 +1,16 @@
+import type { ISiteMetadata } from "../types";
+
+export const siteMetadata: ISiteMetadata = {
+  version: 1,
+  id: "whupt",
+  name: "珞樱-WHUPT",
+  description: "武汉大学自己的武汉大学网络中心提供硬件支持、由学生自主运行与管理的校园资源共享平台",
+  collaborator: ["Rhilip"],
+
+  type: "private",
+  schema: "NexusPHP",
+
+  urls: ["https://whu.pt/"],
+
+  isDead: true, // 于 2019年12月31日正式关站
+};
