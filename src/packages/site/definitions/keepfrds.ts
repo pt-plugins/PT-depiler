f9ba4060d251d9790f4993eb935fba0eb0783d58
fzlins <fzlins2013@gmail.com>
2025-06-29 07:08:34 -0400

    feat(pkg/site): 更新 bonusPerHour 计算逻辑并优化 userDetails 处理

--- src/packages/site/definitions/keepfrds.ts
+++ src/packages/site/definitions/keepfrds.ts
@@ -208,33 +208,49 @@ export const siteMetadata: ISiteMetadata = {
         filters: [{ name: "parseSize" }],
       },
       bonusPerHour: {
-        selector: ["td.embedded:has(>i.fab.fa-btc)"],
+        selector: ["tbody:has(>tr>td.embedded>i.fab.fa-btc)"],
         filters: [
           (query: string | number) => {
             const queryMatch = String(query || "")
               .replace(/,/g, "")
               .match(/[\d.]+/g);
             if (!queryMatch) return 0;
-            const bonusPerHour =
-              queryMatch.length === 4
-                ? parseFloat(queryMatch[1]) + parseFloat(queryMatch[3])
-                : parseFloat(queryMatch[1]);
+            let bonusPerHour = 0;
+            if (queryMatch.length === 5) {
+              bonusPerHour = parseFloat(queryMatch[2]) + parseFloat(queryMatch[4]);
+            } else if (queryMatch.length >= 3) {
+              bonusPerHour = parseFloat(queryMatch[2]);
+            }
             return bonusPerHour;
           },
         ],
       },
     },
     process: [
-      ...SchemaMetadata.userInfo!.process!.filter((item) => item.requestConfig.url !== "/mybonus.php").map((item) => {
-        // 如果是userdetails.php，添加bonusPerHour字段，这样就能使用我们自定义的selector
-        if (item.requestConfig.url === "/userdetails.php") {
-          return {
-            ...item,
-            fields: [...(item.fields || []), "bonusPerHour"],
-          };
-        }
-        return item;
-      }),
+      {
+        requestConfig: { url: "/index.php", responseType: "document" },
+        fields: ["id", "name"],
+      },
+      {
+        requestConfig: { url: "/userdetails.php", responseType: "document" },
+        assertion: { id: "params.id" },
+        fields: [
+          "messageCount",
+          "uploaded",
+          "trueUploaded",
+          "downloaded",
+          "trueDownloaded",
+          "levelName",
+          "bonus",
+          "seedingBonus",
+          "joinTime",
+          "seeding",
+          "seedingSize",
+          "hnrUnsatisfied",
+          "hnrPreWarning",
+          "bonusPerHour", // 使用我们自定义的 selector 和 filter
+        ],
+      },
     ],
   },
 
