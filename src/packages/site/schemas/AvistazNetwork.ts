31b0aa387c860c3638f84b09f67ff482d2c41446
Rhilip <Rhilip@users.noreply.github.com>
2025-07-02 20:32:15 +0800

    style: Update AvistazNetwork.ts

--- src/packages/site/schemas/AvistazNetwork.ts
+++ src/packages/site/schemas/AvistazNetwork.ts
@@ -11,12 +11,9 @@ import {
   type IUserInfo,
   type ITorrent,
   type ITorrentTag,
-  type ISearchInput
-} from "../types"; 
-import {
-  parseTimeWithZone,
-  parseSizeString
-} from "../utils";
+  type ISearchInput,
+} from "../types";
+import { parseTimeWithZone, parseSizeString } from "../utils";
 
 interface AuthSuccessResp {
   token: string;
@@ -71,12 +68,11 @@ export const SchemaMetadata: Pick<
   ISiteMetadata,
   "version" | "schema" | "type" | "timezoneOffset" | "search" | "userInfo" | "userInputSettingMeta"
 > = {
-
   version: 0,
   schema: "AvistazNetwork",
   type: "private",
   // tzOffset@refs: https://github.com/Jackett/Jackett/blob/master/src/Jackett.Common/Indexers/Definitions/Abstract/AvistazTracker.cs#L28C9-L28C122
-  timezoneOffset: "-0400", 
+  timezoneOffset: "-0400",
 
   search: {
     keywordPath: "params.search",
@@ -91,21 +87,21 @@ export const SchemaMetadata: Pick<
           set(config!, "params.imdb", config!.params.search.replace("tt", ""));
           delete config!.params.search;
           return config!;
-        }
+        },
       },
       tvdb: {
         requestConfigTransformer: ({ requestConfig: config }) => {
           set(config!, "params.tvdb", config!.params.search);
           delete config!.params.search;
           return config!;
-        }
+        },
       },
       tmdb: {
         requestConfigTransformer: ({ requestConfig: config }) => {
           set(config!, "params.tmdb", config!.params.search);
           delete config!.params.search;
           return config!;
-        }
+        },
       },
     },
     selectors: {
@@ -115,13 +111,15 @@ export const SchemaMetadata: Pick<
       subTitle: { text: "" }, // AvzNet不提供subTitle
       url: { selector: "url" },
       link: { selector: "download" },
-      category: { 
+      category: {
         selector: "category",
-        filters: [(category: Record<string, string> | undefined) => {
-          if (!category) return '';
-          const values = Object.values(category);
-          return values.length > 0 ? values[0] : '';
-        }]
+        filters: [
+          (category: Record<string, string> | undefined) => {
+            if (!category) return "";
+            const values = Object.values(category);
+            return values.length > 0 ? values[0] : "";
+          },
+        ],
       },
       time: { selector: "created_at", filters: [{ name: "parseTime" }] },
       size: { selector: "file_size" },
@@ -144,15 +142,25 @@ export const SchemaMetadata: Pick<
     selectors: {
       name: { selector: ["span.user-group.group-member"] },
       levelName: { selector: ["body > header > div.ratio-bar.mb-1.pt-2.pl-2.pb-1 > div > div:nth-child(2)"] },
-      uploaded: { selector: ["body > header > div.ratio-bar.mb-1.pt-2.pl-2.pb-1 > div > div:nth-child(3)"], filters: [{ name: "parseSize" }] },
-      downloaded: { selector: ["body > header > div.ratio-bar.mb-1.pt-2.pl-2.pb-1 > div > div:nth-child(4)"], filters: [{ name: "parseSize" }] },
-      ratio: { selector: ["body > header > div.ratio-bar.mb-1.pt-2.pl-2.pb-1 > div > div:nth-child(5)"], filters: [{ name: "parseNumber" }] },
-      bonus: { selector: ["body > header > div.ratio-bar.mb-1.pt-2.pl-2.pb-1 > div > div:nth-child(9)"], filters: [{ name: "parseNumber" }] },
+      uploaded: {
+        selector: ["body > header > div.ratio-bar.mb-1.pt-2.pl-2.pb-1 > div > div:nth-child(3)"],
+        filters: [{ name: "parseSize" }],
+      },
+      downloaded: {
+        selector: ["body > header > div.ratio-bar.mb-1.pt-2.pl-2.pb-1 > div > div:nth-child(4)"],
+        filters: [{ name: "parseSize" }],
+      },
+      ratio: {
+        selector: ["body > header > div.ratio-bar.mb-1.pt-2.pl-2.pb-1 > div > div:nth-child(5)"],
+        filters: [{ name: "parseNumber" }],
+      },
+      bonus: {
+        selector: ["body > header > div.ratio-bar.mb-1.pt-2.pl-2.pb-1 > div > div:nth-child(9)"],
+        filters: [{ name: "parseNumber" }],
+      },
       joinTime: {
         selector: ["table.table-striped tr:contains('Joined') td:last-child"],
-        filters: [
-          { "name": "parseTime", "args": ["dd MMMM yyyy hh:mm a"] }
-        ]
+        filters: [{ name: "parseTime", args: ["dd MMMM yyyy hh:mm a"] }],
       },
       uploads: { selector: [".card .tag-green"], filters: [{ name: "parseNumber" }] },
       snatches: { selector: [".card .tag-yellow"], filters: [{ name: "parseNumber" }] },
@@ -165,34 +173,32 @@ export const SchemaMetadata: Pick<
       authExpiry: { selector: ["expiry"] },
     */
   },
- 
+
   userInputSettingMeta: [
     {
       name: "username",
       label: "Username",
-      hint: "Fill with your username." +
-      "Please Make Sure your RANK >= Member",
+      hint: "Fill with your username." + "Please Make Sure your RANK >= Member",
       required: true,
     },
     {
       name: "password",
       label: "Password",
-      hint: "Fill with your password" +
-      "Please confirm enable ‘Enable RSS Feed’ in account settings",
+      hint: "Fill with your password" + "Please confirm enable ‘Enable RSS Feed’ in account settings",
       required: true,
     },
     {
       name: "pid",
       label: "PID",
-      hint: "Find in Profile Site, reset in Account Setting Site if you want to reset." +
-      "PID is like your password, you must keep it safe!",
+      hint:
+        "Find in Profile Site, reset in Account Setting Site if you want to reset." +
+        "PID is like your password, you must keep it safe!",
       required: true,
     },
   ],
 };
 
 export default class AvistazNetwork extends PrivateSite {
-
   /*
     应站点要求，不启用用户数据获取
     > User information will never be available in any form or API, as we respect the privacy and confidentiality of user information.
@@ -201,13 +207,13 @@ export default class AvistazNetwork extends PrivateSite {
   public override get allowQueryUserInfo(): boolean {
     return false;
   }
-  
+
   public override async getUserInfoResult(lastUserInfo: Partial<IUserInfo> = {}): Promise<IUserInfo> {
     let flushUserInfo: IUserInfo = {
       status: EResultParseStatus.unknownError,
       updateAt: +new Date(),
       site: this.metadata.id,
-    }; 
+    };
 
     if (!this.allowQueryUserInfo) {
       flushUserInfo.status = EResultParseStatus.passParse;
@@ -215,40 +221,40 @@ export default class AvistazNetwork extends PrivateSite {
       flushUserInfo.levelName = "应站点要求，不启用用户数据获取";
       return flushUserInfo;
     }
-  /* 预留获取用户信息
-    // 对 AvistazNetwork，如果定义了 process，则按照 AbstractPrivateSite 的方式处理
-    if (Array.isArray(this.metadata.userInfo?.process)) {
-      return await super.getUserInfoResult(lastUserInfo);
-    }
-
-    try {
-      flushUserInfo = { ...flushUserInfo, ...(await this.getBaseInfoFromSite()) };
-      if (flushUserInfo.name) {
-        flushUserInfo = {
-          ...flushUserInfo,
-          ...(await this.getExtendInfoFromProfile(flushUserInfo.name as string)),
-          ...(await this.getUserSeedingTorrents(flushUserInfo.name as string)),
-        };
-      }
-      else {
-        flushUserInfo.name = this.userConfig.inputSetting?.username;
-        flushUserInfo = {
-          ...flushUserInfo,
-          ...(await this.getExtendInfoFromProfile(flushUserInfo.name as string)),
-          ...(await this.getUserSeedingTorrents(flushUserInfo.name as string)),
-        };
+    /* 预留获取用户信息
+      // 对 AvistazNetwork，如果定义了 process，则按照 AbstractPrivateSite 的方式处理
+      if (Array.isArray(this.metadata.userInfo?.process)) {
+        return await super.getUserInfoResult(lastUserInfo);
       }
 
-      if (this.metadata.levelRequirements && flushUserInfo.levelName && typeof flushUserInfo.levelId === "undefined") {
-        flushUserInfo.levelId = this.guessUserLevelId(flushUserInfo as IUserInfo);
+      try {
+        flushUserInfo = { ...flushUserInfo, ...(await this.getBaseInfoFromSite()) };
+        if (flushUserInfo.name) {
+          flushUserInfo = {
+            ...flushUserInfo,
+            ...(await this.getExtendInfoFromProfile(flushUserInfo.name as string)),
+            ...(await this.getUserSeedingTorrents(flushUserInfo.name as string)),
+          };
+        }
+        else {
+          flushUserInfo.name = this.userConfig.inputSetting?.username;
+          flushUserInfo = {
+            ...flushUserInfo,
+            ...(await this.getExtendInfoFromProfile(flushUserInfo.name as string)),
+            ...(await this.getUserSeedingTorrents(flushUserInfo.name as string)),
+          };
+        }
+
+        if (this.metadata.levelRequirements && flushUserInfo.levelName && typeof flushUserInfo.levelId === "undefined") {
+          flushUserInfo.levelId = this.guessUserLevelId(flushUserInfo as IUserInfo);
+        }
+
+        flushUserInfo.status = EResultParseStatus.success;
+      } catch (error) {
+        flushUserInfo.status = EResultParseStatus.parseError;
       }
-    
-      flushUserInfo.status = EResultParseStatus.success;
-    } catch (error) {
-      flushUserInfo.status = EResultParseStatus.parseError;
-    }
-  */
-    return flushUserInfo; 
+    */
+    return flushUserInfo;
   }
 
   protected async getBaseInfoFromSite(): Promise<Partial<IUserInfo>> {
@@ -257,11 +263,14 @@ export default class AvistazNetwork extends PrivateSite {
       responseType: "document",
     });
 
-    return this.getFieldsData(
-      pageDocument,
-      this.metadata.userInfo?.selectors!,
-      ["name", "levelName", "uploaded", "downloaded", "ratio", "bonus"]
-    ) as Partial<IUserInfo>;
+    return this.getFieldsData(pageDocument, this.metadata.userInfo?.selectors!, [
+      "name",
+      "levelName",
+      "uploaded",
+      "downloaded",
+      "ratio",
+      "bonus",
+    ]) as Partial<IUserInfo>;
   }
 
   protected async getExtendInfoFromProfile(userName: string): Promise<Partial<IUserInfo>> {
@@ -270,11 +279,13 @@ export default class AvistazNetwork extends PrivateSite {
       responseType: "document",
     });
 
-    return this.getFieldsData(
-      pageDocument,
-      this.metadata.userInfo?.selectors!,
-      ["joinTime", "uploads", "snatches", "seeding", "hnrUnsatisfied"]
-    ) as Partial<IUserInfo>;
+    return this.getFieldsData(pageDocument, this.metadata.userInfo?.selectors!, [
+      "joinTime",
+      "uploads",
+      "snatches",
+      "seeding",
+      "hnrUnsatisfied",
+    ]) as Partial<IUserInfo>;
   }
 
   protected async getUserSeedingTorrents(userName: string): Promise<Partial<IUserInfo>> {
@@ -291,54 +302,53 @@ export default class AvistazNetwork extends PrivateSite {
 
     return userSeedingTorrent;
   }
-  
-  public override async request<T>(
-    axiosConfig: AxiosRequestConfig, 
-    checkLogin: boolean = true
-  ): Promise<AxiosResponse<T>> {
 
-		if (axiosConfig.url === "/api/v1/jackett/auth") {
-			axiosConfig.method = "POST";
+  public override async request<T>(
+    axiosConfig: AxiosRequestConfig,
+    checkLogin: boolean = true,
+  ): Promise<AxiosResponse<T>> {
+    if (axiosConfig.url === "/api/v1/jackett/auth") {
+      axiosConfig.method = "POST";
       axiosConfig.data = {
         ...axiosConfig.data,
         username: this.userConfig.inputSetting?.username ?? "",
-				password: this.userConfig.inputSetting?.password ?? "",
-				pid: this.userConfig.inputSetting?.pid ?? "",
-			};
+        password: this.userConfig.inputSetting?.password ?? "",
+        pid: this.userConfig.inputSetting?.pid ?? "",
+      };
       axiosConfig.headers = {
-				...(axiosConfig.headers ?? {}),
+        ...(axiosConfig.headers ?? {}),
         "Content-Type": "application/x-www-form-urlencoded",
-			};
+      };
     }
-		if (axiosConfig.url?.startsWith("/api/v1/jackett/torrents")) {
-			axiosConfig.method = "GET";
+    if (axiosConfig.url?.startsWith("/api/v1/jackett/torrents")) {
+      axiosConfig.method = "GET";
       const apiAuth = await this.getAuthToken();
-      if ('token' in apiAuth) {
+      if ("token" in apiAuth) {
         // If 'token' exists, TypeScript now knows authResponse is AuthSuccessResp
         axiosConfig.headers = {
           ...(axiosConfig.headers ?? {}),
-          "Authorization": `Bearer ${apiAuth.token ?? ""}`,
+          Authorization: `Bearer ${apiAuth.token ?? ""}`,
         };
       } else {
         throw new Error(`Authentication failed: ${apiAuth.message || "Unknown error"}`);
       }
-		}
-		return super.request<T>(axiosConfig, checkLogin);
+    }
+    return super.request<T>(axiosConfig, checkLogin);
   }
 
   // TODO：为减少token获取次数，预留函数
   /*
   public async getAuthToken(): Promise<{ token: string; expiry: number }> {
   // 检查当前token是否存在且未过期
-    if (this.metadata.userInfo?.selectors?.authToken && 
-        this.metadata.userInfo?.selectors?.authExpiry && 
+    if (this.metadata.userInfo?.selectors?.authToken &&
+        this.metadata.userInfo?.selectors?.authExpiry &&
         this.metadata.userInfo.selectors.authExpiry > Math.floor(Date.now() / 1000)) {
       return {
         token: this.metadata.userInfo.selectors.authToken,
         expiry: this.metadata.userInfo.selectors.authExpiry
       };
     }
-  
+
   // 请求新token
     const { data: apiAuth } = await this.request<AvzNetAuthResp>(
       {
@@ -363,37 +373,35 @@ export default class AvistazNetwork extends PrivateSite {
   }
   */
   public async getAuthToken(): Promise<AvzNetAuthResp> {
-    const { data: apiAuth } = await this.request<AvzNetAuthResp>(
-      {
-        url: "/api/v1/jackett/auth",
-        responseType: "json",
-      },
-    );
+    const { data: apiAuth } = await this.request<AvzNetAuthResp>({
+      url: "/api/v1/jackett/auth",
+      responseType: "json",
+    });
 
     return apiAuth ?? [];
   }
-  
+
   protected override parseTorrentRowForTags(
-		torrent: Partial<ITorrent>,
-		row: IAvzNetRawTorrent,
-		searchConfig: ISearchInput,
-	): Partial<ITorrent> {
-		const tags: ITorrentTag[] = [];
+    torrent: Partial<ITorrent>,
+    row: IAvzNetRawTorrent,
+    searchConfig: ISearchInput,
+  ): Partial<ITorrent> {
+    const tags: ITorrentTag[] = [];
 
-		const { upload_multiply, download_multiply } = row as { upload_multiply?: number; download_multiply?: number };
-		if (upload_multiply === 2) {
-			tags.push({ name: `${upload_multiply}xUp`, color: "lime" });
-		}
-		switch (download_multiply) {
-			case 0:
-				tags.push({ name: "Free", color: "blue" });
-				break;
-			case 0.5:
-				tags.push({ name: "50%", color: "deep-orange-darken-1" });
-				break;
-		}
+    const { upload_multiply, download_multiply } = row as { upload_multiply?: number; download_multiply?: number };
+    if (upload_multiply === 2) {
+      tags.push({ name: `${upload_multiply}xUp`, color: "lime" });
+    }
+    switch (download_multiply) {
+      case 0:
+        tags.push({ name: "Free", color: "blue" });
+        break;
+      case 0.5:
+        tags.push({ name: "50%", color: "deep-orange-darken-1" });
+        break;
+    }
 
-		torrent.tags = tags;
-		return torrent;
-	}
+    torrent.tags = tags;
+    return torrent;
+  }
 }
