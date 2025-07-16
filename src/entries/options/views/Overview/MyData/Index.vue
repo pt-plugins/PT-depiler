a14b14d76232765d988eef9ec1fa47c78c40e447
Rhilip <admin@rhilip.info>
2025-07-17 00:06:45 +0800

    fix(MyData): simplify seeding and HnR display logic and improve title binding

--- src/entries/options/views/Overview/MyData/Index.vue
+++ src/entries/options/views/Overview/MyData/Index.vue
@@ -521,17 +521,11 @@ function viewStatistic() {
 
       <!-- 做种数， H&R 情况  -->
       <template #item.seeding="{ item }">
-        <v-container
-          v-if="
-            configStore.myDataTableControl.showHnR &&
-            ((typeof item.hnrPreWarning !== 'undefined' && item.hnrPreWarning > 0) ||
-              (typeof item.hnrUnsatisfied !== 'undefined' && item.hnrUnsatisfied > 0))
-          "
-        >
-          <v-row align="center" class="flex-nowrap" justify="end">
+        <v-container class="py-0">
+          <v-row align="center" class="flex-nowrap my-0" justify="end">
             <span class="text-no-wrap">{{ item.seeding ?? "-" }}</span>
           </v-row>
-          <v-row align="center" class="flex-nowrap" justify="end">
+          <v-row v-if="configStore.myDataTableControl.showHnR" align="center" class="flex-nowrap my-0" justify="end">
             <span
               v-if="typeof item.hnrPreWarning !== 'undefined' && item.hnrPreWarning > 0"
               class="d-inline-flex align-center ml-2"
@@ -562,7 +556,6 @@ function viewStatistic() {
             </span>
           </v-row>
         </v-container>
-        <span v-else class="text-no-wrap">{{ item.seeding ?? "-" }}</span>
       </template>
 
       <!-- 做种量 -->
@@ -582,14 +575,14 @@ function viewStatistic() {
           "
         >
           <v-row align="center" class="flex-nowrap" justify="end">
-            <v-icon title="t('levelRequirement.bonus')" color="green-darken-4" icon="mdi-currency-usd" size="small" />
+            <v-icon :title="t('levelRequirement.bonus')" color="green-darken-4" icon="mdi-currency-usd" size="small" />
             <span class="text-no-wrap">
               {{ typeof item.bonus !== "undefined" ? formatNumber(item.bonus) : "-" }}
             </span>
           </v-row>
           <v-row align="center" class="flex-nowrap" justify="end">
             <v-icon
-              title="t('levelRequirement.seedingBonus')"
+              :title="t('levelRequirement.seedingBonus')"
               color="green-darken-4"
               icon="mdi-lightning-bolt-circle"
               size="small"
@@ -600,7 +593,7 @@ function viewStatistic() {
           </v-row>
         </v-container>
         <template v-else>
-          <v-icon title="t('levelRequirement.bonus')" color="green-darken-4" icon="mdi-currency-usd" size="small" />
+          <v-icon :title="t('levelRequirement.bonus')" color="green-darken-4" icon="mdi-currency-usd" size="small" />
           <span class="text-no-wrap">
             {{ typeof item.bonus !== "undefined" ? formatNumber(item.bonus) : "-" }}
           </span>
