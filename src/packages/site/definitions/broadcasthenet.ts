276789314e9d2db9faf3acabcab138acbfb28e3e
fzlins <fzlins2013@gmail.com>
2025-07-17 11:05:49 -0400

    feat(site): 新增 BTN 站点分区定义

---
+++ src/packages/site/definitions/broadcasthenet.ts
@@ -0,0 +1,136 @@
+import type { ISiteMetadata } from "../types.ts";
+import { SchemaMetadata } from "../schemas/Gazelle.ts";
+
+export const siteMetadata: ISiteMetadata = {
+  ...SchemaMetadata,
+
+  version: 1,
+  id: "broadcasthenet",
+  name: "BroadcastTheNet",
+  aka: ["BTN"],
+  description: "BroadcastTheNet is a private tracker for TV shows.",
+  tags: ["电视剧"],
+  timezoneOffset: "+0000",
+  collaborator: ["Sunhelter"],
+
+  type: "private",
+  schema: "Gazelle",
+
+  urls: ["uggcf://oebnqpnfgur.arg/"],
+
+  userInfo: {
+    ...SchemaMetadata.userInfo!,
+    selectors: {
+      ...SchemaMetadata.userInfo!.selectors,
+      uploaded: {
+        selector: "ul.nobullet > li:contains('Upload:')",
+        filters: [{ name: "parseSize" }],
+      },
+      uploads: {
+        selector: "ul.nobullet > li:contains('Uploaded:')",
+        filters: [{ name: "parseNumber" }],
+      },
+      downloaded: {
+        selector: "ul.nobullet > li:contains('Downloaded:')",
+        filters: [{ name: "parseSize" }],
+      },
+      levelName: {
+        selector: "ul.nobullet > li:contains('User Class:')",
+        filters: [
+          (query: string) => {
+            return query.replace("User Class:", "").trim();
+          },
+        ],
+      },
+      bonus: {
+        selector: "ul.nobullet > li:contains('Bonus Points:') > a",
+        filters: [{ name: "parseNumber" }],
+      },
+      ratio: undefined,
+      joinTime: {
+        selector: "ul.nobullet > li:contains('Joined:') > span",
+        filters: [{ name: "parseTTL" }],
+      },
+      seeding: {
+        selector: "ul.nobullet > li:contains('Seeding:')",
+        filters: [{ name: "parseNumber" }],
+      },
+      seedingSize: {
+        selector: "ul.nobullet > li:contains('Seeding Size:')",
+        filters: [{ name: "parseSize" }],
+      },
+      totalTraffic: {
+        selector: "ul.nobullet > li:contains('Total Traffic:')",
+        filters: [{ name: "parseSize" }],
+      },
+    },
+  },
+
+  levelRequirements: [
+    {
+      id: 1,
+      name: "Member",
+      totalTraffic: "100GB",
+      bonus: 100000,
+      snatches: 100,
+      interval: "P2W",
+      privilege: "Can access the XXX forum.",
+    },
+    {
+      id: 2,
+      name: "Power User",
+      totalTraffic: "250GB",
+      bonus: 250000,
+      snatches: 250,
+      interval: "P4W",
+      privilege:
+        "Has access to the Power User forum, Official and Unofficial Invites forums, Top 10 filters, and can access notifications.",
+    },
+    {
+      id: 3,
+      name: "Extreme User",
+      totalTraffic: "500GB",
+      bonus: 500000,
+      snatches: 500,
+      interval: "P2M3W",
+      privilege: "Has access to the Extreme User forum.",
+    },
+    {
+      id: 4,
+      name: "Elite",
+      totalTraffic: "1TB",
+      bonus: 850000,
+      snatches: 1000,
+      interval: "P5M2W",
+      privilege:
+        "Has access to the Elite forum and can set own Custom Title, and the ability to send invites purchased from the Lumens Store.",
+    },
+    {
+      id: 5,
+      name: "Guru",
+      totalTraffic: "2.5TB",
+      bonus: 1500000,
+      snatches: 1500,
+      interval: "P8M1W",
+      privilege: "Has access to the Guru forum.",
+    },
+    {
+      id: 6,
+      name: "Master",
+      totalTraffic: "7.5TB",
+      bonus: 3000000,
+      snatches: 3000,
+      interval: "P11M4W",
+      privilege: "Has access to the Master forum.",
+    },
+    {
+      id: 7,
+      name: "Overlord",
+      totalTraffic: "100TB",
+      bonus: 250000000,
+      snatches: 35000,
+      interval: "P2Y11M",
+      privilege: "Has access to the Overlord forum, Custom Title, Unlimited Invites, and more to come!",
+    },
+  ],
+};
