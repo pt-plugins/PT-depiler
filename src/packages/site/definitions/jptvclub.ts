817bedc0d6daa5f64ef1ee2d2471c16b11e957ce
fzlins <fzlins2013@gmail.com>
2025-07-30 09:48:24 -0400

    Add JPTV dead site definition

---
+++ src/packages/site/definitions/jptvclub.ts
@@ -0,0 +1,16 @@
+import type { ISiteMetadata } from "../types";
+
+export const siteMetadata: ISiteMetadata = {
+  version: 1,
+  id: "jptvclub",
+  name: "JPTVClub",
+  description: "JPTV.club 是一个专注于日本电视剧、电影和 FANRES（粉丝录像）资源的私有高清种子社区库",
+  tags: ["影视", "剧集", "动漫"],
+
+  type: "private",
+  schema: "UNIT3D",
+
+  urls: ["https://jptv.club/"],
+
+  isDead: true,
+};
