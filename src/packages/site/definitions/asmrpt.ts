d311f16c483773c8daa3cb41fcec3d539fedb013
Rhilip <admin@rhilip.info>
2025-08-19 20:57:27 +0800

    feat(pkg/site): 使用NPHP默认适配站点 asmrpt

---
+++ src/packages/site/definitions/asmrpt.ts
@@ -0,0 +1,91 @@
+import { type ISiteMetadata } from "../types";
+import { SchemaMetadata } from "../schemas/NexusPHP.ts";
+
+export const siteMetadata: ISiteMetadata = {
+  ...SchemaMetadata,
+
+  version: 1,
+  id: "asmrpt",
+  name: "ASMR PT",
+  description: "致力于成为最全ASMR PT社区",
+  tags: ["视频", "ASMR"],
+  timezoneOffset: "+0800",
+
+  type: "private",
+  schema: "NexusPHP",
+
+  urls: ["https://www.asmrpt.org/"],
+
+  levelRequirements: [
+    {
+      id: 0,
+      name: "User",
+      privilege: "新用户的默认级别；只能在每周六中午12点至每周日晚上11点59分发布种子",
+    },
+    {
+      id: 1,
+      name: "Power User",
+      interval: "P4W",
+      downloaded: "50GB",
+      ratio: 1.05,
+      privilege:
+        '得到一个邀请名额；可以直接发布种子；可以查看NFO文档；可以查看用户列表；可以请求续种； 可以发送邀请；可以查看排行榜；可以查看其它用户的种子历史(如果用户隐私等级未设置为"强")； 可以删除自己上传的字幕。',
+    },
+    {
+      id: 2,
+      name: "Elite User",
+      interval: "P8W",
+      downloaded: "120GB",
+      ratio: 1.55,
+      privilege: "Elite User及以上用户封存账号后不会被删除。",
+    },
+    {
+      id: 3,
+      name: "Crazy User",
+      interval: "P15W",
+      downloaded: "300GB",
+      ratio: 2.05,
+      privilege: "得到两个邀请名额；可以在做种/下载/发布的时候选择匿名模式。",
+    },
+    {
+      id: 4,
+      name: "Insane User",
+      interval: "P25W",
+      downloaded: "500GB",
+      ratio: 2.55,
+      privilege: "可以查看普通日志。",
+    },
+    {
+      id: 5,
+      name: "Veteran User",
+      interval: "P40W",
+      downloaded: "750GB",
+      ratio: 3.05,
+      privilege: "得到三个邀请名额；可以查看其它用户的评论、帖子历史。Veteran User及以上用户会永远保留账号。",
+    },
+    {
+      id: 6,
+      name: "Extreme User",
+      interval: "P60W",
+      downloaded: "1TB",
+      ratio: 3.55,
+      privilege: "可以更新过期的外部信息；可以查看Extreme User论坛。",
+    },
+    {
+      id: 7,
+      name: "Ultimate User",
+      interval: "P80W",
+      downloaded: "1.5TB",
+      ratio: 4.05,
+      privilege: "得到五个邀请名额。",
+    },
+    {
+      id: 8,
+      name: "Nexus Master",
+      interval: "P100W",
+      downloaded: "3TB",
+      ratio: 4.55,
+      privilege: "得到十个邀请名额。",
+    },
+  ],
+};
