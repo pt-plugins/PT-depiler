fcb07d72d2ba86daeaf463b1bcd61cc05c1c4812
Rhilip <admin@rhilip.info>
2025-08-20 19:29:22 +0800

    feat(downloader): 允许使用 <...> 实现快速推送时自定义savePath, label

--- src/entries/options/components/SentToDownloaderDialog.vue
+++ src/entries/options/components/SentToDownloaderDialog.vue
@@ -83,6 +83,11 @@ async function sendToDownloader() {
   isSending.value = true;
   const promises = [];
 
+  const customReplace = {
+    savePath: undefined,
+    label: undefined,
+  } as Record<string, string | undefined>;
+
   for (const torrent of torrentItems) {
     const realAddTorrentOptions: Partial<CAddTorrentOptions> = { ...addTorrentOptions.value };
 
@@ -106,6 +111,18 @@ async function sendToDownloader() {
           for (const [replaceKey, value] of Object.entries(replaceMap)) {
             // @ts-ignore
             realAddTorrentOptions[key] = (realAddTorrentOptions[key]! as string).replace(`$${replaceKey}$`, value);
+          }
+
+          // 处理自定义输入
+          if ((realAddTorrentOptions[key] as string).includes("<...>")) {
+            // 如果之前已经输入过，则直接使用之前的输入
+            if (customReplace[key] == undefined) {
+              const userInput = prompt(`请输入替换 ${key} 中的 <...> 的内容：`, "");
+              customReplace[key] = userInput ? userInput.trim() : "";
+            }
+
+            // @ts-ignore
+            realAddTorrentOptions[key] = (realAddTorrentOptions[key] as string).replace("<...>", customReplace[key]!);
           }
         }
       }
