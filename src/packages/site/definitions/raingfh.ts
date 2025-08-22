39124e0266b00a559828d96e1f4d4c615c69f3ec
Hui-Shao <31568606+hui-shao@users.noreply.github.com>
2025-08-22 14:14:13 +0800

    fix(pkg/site): raingfh 缺少 User 等级 (#467)

--- src/packages/site/definitions/raingfh.ts
+++ src/packages/site/definitions/raingfh.ts
@@ -55,6 +55,11 @@ export const siteMetadata: ISiteMetadata = {
   levelRequirements: [
     {
       id: 1,
+      name: "User",
+      privilege: "新用户的默认等级",
+    },
+    {
+      id: 2,
       name: "Power User（灯火通明）",
       interval: "P4W",
       downloaded: "50GB",
@@ -64,7 +69,7 @@ export const siteMetadata: ISiteMetadata = {
         '得到一个邀请名额；可以查看NFO文档；可以查看用户列表；可以请求续种； 可以发送邀请； 可以查看排行榜；可以查看其它用户的种子历史(如果用户隐私等级未设置为"强")； 可以删除自己上传的字幕。',
     },
     {
-      id: 2,
+      id: 3,
       name: "Elite User（月华如水）",
       interval: "P8W",
       downloaded: "120GB",
@@ -73,7 +78,7 @@ export const siteMetadata: ISiteMetadata = {
       privilege: "User及以上用户封存账号后不会被删除。",
     },
     {
-      id: 3,
+      id: 4,
       name: "Crazy User（流光溢彩）",
       interval: "P15W",
       downloaded: "300GB",
@@ -82,7 +87,7 @@ export const siteMetadata: ISiteMetadata = {
       privilege: "得到两个邀请名额；可以在做种/下载/发布的时候选择匿名模式。",
     },
     {
-      id: 4,
+      id: 5,
       name: "Insane User（光芒万丈）",
       interval: "P25W",
       downloaded: "500GB",
@@ -91,7 +96,7 @@ export const siteMetadata: ISiteMetadata = {
       privilege: "可以查看普通日志。",
     },
     {
-      id: 5,
+      id: 6,
       name: "Veteran User（星火燎原）",
       interval: "P40W",
       downloaded: "750GB",
@@ -100,7 +105,7 @@ export const siteMetadata: ISiteMetadata = {
       privilege: "得到三个邀请名额；可以查看其它用户的评论、帖子历史。Veteran User及以上用户会永远保留账号。",
     },
     {
-      id: 6,
+      id: 7,
       name: "Extreme User（光耀四方）",
       interval: "P60W",
       downloaded: "1TB",
@@ -109,7 +114,7 @@ export const siteMetadata: ISiteMetadata = {
       privilege: "可以更新过期的外部信息；可以查看Extreme User论坛。",
     },
     {
-      id: 7,
+      id: 8,
       name: "Ultimate User（烈日当空）",
       interval: "P80W",
       downloaded: "1.5TB",
@@ -118,7 +123,7 @@ export const siteMetadata: ISiteMetadata = {
       privilege: "得到五个邀请名额。",
     },
     {
-      id: 8,
+      id: 9,
       name: "Nexus Master（星河璀璨）",
       interval: "P100W",
       downloaded: "3TB",
