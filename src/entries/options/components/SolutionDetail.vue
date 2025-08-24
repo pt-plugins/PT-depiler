1ffeed0ced0e4726abb860a18b6a1f32d3e3174b
Rhilip <Rhilip@users.noreply.github.com>
2025-08-24 12:27:32 +0800

    feat(SetSearchSolution): 允许自定义搜索方案内容 (#475)

--- src/entries/options/components/SolutionDetail.vue
+++ src/entries/options/components/SolutionDetail.vue
@@ -1,49 +1,41 @@
 <script setup lang="ts">
-import { computedAsync } from "@vueuse/core";
+import { onMounted, shallowRef } from "vue";
 import { isEmpty } from "es-toolkit/compat";
 import type { ISearchCategories } from "@ptd/site";
 
-import { useMetadataStore } from "@/options/stores/metadata.ts";
 import type { ISearchSolution } from "@/shared/types.ts";
+
+import {
+  getCategoryName,
+  getCategoryOptionName,
+  getSiteMetaCategory,
+} from "@/options/views/Settings/SetSearchSolution/utils.ts";
 
 const props = defineProps<{
   solution: ISearchSolution;
 }>();
 
-const metadataStore = useMetadataStore();
+const siteMetaCategory = shallowRef<ISearchCategories[]>([]);
 
-const siteMetaCategory = computedAsync(async () => {
-  return (await metadataStore.getSiteMergedMetadata(props.solution.siteId, "category", [])) as ISearchCategories[];
-}, []);
-
-function getCategory(key: string) {
-  return siteMetaCategory.value.find((x) => x.key === key) ?? { key, name: key, options: [] };
-}
-
-function getCategoryName(key: string) {
-  return getCategory(key)?.name ?? key;
-}
-
-function getCategoryOptionName(key: string, value: string | number | (string | number)[]) {
-  const options = getCategory(key)?.options ?? [];
-  if (Array.isArray(value)) {
-    return value.map((v) => options.find((o) => o.value === v)?.name ?? v).join(", ");
-  } else {
-    return options.find((o) => o.value === value)?.name ?? value;
-  }
-}
+onMounted(async () => {
+  siteMetaCategory.value = await getSiteMetaCategory(props.solution.siteId);
+});
 </script>
 
 <template>
   <div v-if="solution" class="text-wrap">
-    <template v-if="isEmpty(solution.selectedCategories)">默认</template>
+    <template v-if="solution.name">{{ solution.name }}</template>
+    <template v-else-if="isEmpty(solution.selectedCategories)">默认</template>
     <template v-else>
       <span
         v-for="(value, category) in solution.selectedCategories"
         :key="category"
-        :title="getCategoryName(category) + ': ' + getCategoryOptionName(category, value)"
+        :title="
+          getCategoryName(siteMetaCategory, category) + ': ' + getCategoryOptionName(siteMetaCategory, category, value)
+        "
       >
-        <b>{{ getCategoryName(category) }}</b> : {{ getCategoryOptionName(category, value) }};&nbsp;
+        <b>{{ getCategoryName(siteMetaCategory, category) }}</b> :
+        {{ getCategoryOptionName(siteMetaCategory, category, value) }};&nbsp;
       </span>
     </template>
   </div>
