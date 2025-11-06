0366c093791b5bc1374e94ea5aa346e80ca80cf6
akina-up <akina2up@proton.me>
2025-11-06 23:02:57 +0800

    feat(pkg/site): add cinematik

---
+++ src/packages/site/definitions/cinematik.ts
@@ -0,0 +1,175 @@
+import { type ISiteMetadata } from "../types";
+import { SchemaMetadata } from "../schemas/Unit3D.ts";
+
+export const siteMetadata: ISiteMetadata = {
+  ...SchemaMetadata,
+  version: 1,
+  id: "cinematik",
+  name: "Cinematik",
+  aka: ["tik"],
+  tags: ["影视,原盘"],
+  timezoneOffset: "+0000",
+  collaborator: ["akina"],
+
+  type: "private",
+  schema: "Unit3D",
+
+  urls: ["uggcf://pvarzngvx.arg/"],
+
+  levelRequirements: [
+    {
+      id: 1,
+      name: "User",
+      privilege: "2下载槽",
+    },
+    {
+      id: 2,
+      name: "CineNovice",
+      ratio: 0.95,
+      interval: "P30D",
+      uploaded: "1TiB",
+      privilege: "10下载槽",
+    },
+    {
+      id: 3,
+      name: "FilmEnthusiast",
+      ratio: 1.05,
+      interval: "P60D",
+      uploaded: "5TiB",
+      privilege: "15下载槽 访问邀请区",
+    },
+    {
+      id: 4,
+      name: "FilmScholar",
+      ratio: 1.2,
+      interval: "P90D",
+      uploaded: "10TiB",
+      privilege: "20下载槽 访问邀请区",
+    },
+    {
+      id: 5,
+      name: "CineExpert",
+      ratio: 1.5,
+      interval: "P180D",
+      uploaded: "50TiB",
+      privilege: "25下载槽 访问邀请区 发送邀请",
+    },
+    {
+      id: 6,
+      name: "CinemaSage",
+      ratio: 2,
+      interval: "P365D",
+      uploaded: "100TiB",
+      privilege: "30下载槽 访问邀请区 发送邀请",
+    },
+    {
+      id: 7,
+      name: "CineSeeder",
+      ratio: 1,
+      interval: "P60D",
+      averageSeedingTime: "P60D",
+      seedingSize: "10TiB",
+      privilege: "50下载槽 访问邀请区 发送邀请 Personalised HnR management by staff",
+    },
+    {
+      id: 8,
+      name: "FilmArchivist",
+      ratio: 1,
+      interval: "P90D",
+      averageSeedingTime: "P90D",
+      seedingSize: "20TiB",
+      privilege: "1000下载槽 访问邀请区 发送邀请 免疫HR 双倍上传 可以访问FilmArchivist论坛",
+    },
+  ],
+
+  category: [
+    {
+      name: "类别",
+      key: "categoryIds",
+      options: [
+        { name: "Film", value: 1 },
+        { name: "TV", value: 2 },
+        { name: "Foreign Film", value: 3 },
+        { name: "Foreign TV", value: 4 },
+        { name: "Opera &amp; Musical", value: 5 },
+        { name: "Asian Film", value: 6 },
+      ],
+      cross: { mode: "brackets" },
+    },
+    {
+      name: "规格",
+      key: "typeIds",
+      options: [
+        { name: "UHD BD100", value: 3 },
+        { name: "UHD BD66", value: 4 },
+        { name: "BD50", value: 5 },
+        { name: "BD25", value: 6 },
+        { name: "NTSC DVD9", value: 7 },
+        { name: "NTSC DVD5", value: 8 },
+        { name: "PAL DVD9", value: 9 },
+        { name: "PAL DVD5", value: 10 },
+        { name: "Custom", value: 1 },
+        { name: "3D", value: 11 },
+      ],
+      cross: { mode: "brackets" },
+    },
+    {
+      name: "分辨率",
+      key: "resolutionIds",
+      options: [
+        { name: "2160p", value: 2 },
+        { name: "1080p", value: 3 },
+        { name: "1080i", value: 4 },
+        { name: "720p", value: 5 },
+        { name: "576p", value: 6 },
+        { name: "576i", value: 7 },
+        { name: "480p", value: 8 },
+        { name: "480i", value: 9 },
+        { name: "Other", value: 10 },
+      ],
+      cross: { mode: "brackets" },
+    },
+    {
+      name: "Buff",
+      key: "free",
+      options: [
+        { name: "0% Freeleech", value: 0 },
+        { name: "25% Free", value: 25 },
+        { name: "50% Free", value: 50 },
+        { name: "75% Free", value: 75 },
+        { name: "100% Free", value: 100 },
+        { name: "双倍上传", value: "doubleup" },
+        { name: "精选", value: "featured" },
+        { name: "Refundable", value: "refundable" },
+      ],
+      cross: { mode: "custom" },
+      generateRequestConfig: (selectedOptions) => {
+        const params: Record<string, any> = { free: [] };
+        (selectedOptions as Array<number | string>).forEach((value) => {
+          if (value === "doubleup" || value === "featured" || value === "refundable") {
+            params[value] = 1;
+          } else {
+            params.free.push(value);
+          }
+        });
+        return { requestConfig: { params } };
+      },
+    },
+  ],
+
+  search: {
+    ...SchemaMetadata.search,
+    skipNonLatinCharacters: true,
+    selectors: {
+      ...SchemaMetadata.search!.selectors,
+      tags: [
+        ...SchemaMetadata.search!.selectors!.tags!,
+        {
+          name: "H&R",
+          selector: "*",
+          color: "red",
+        },
+      ],
+    },
+  },
+};
