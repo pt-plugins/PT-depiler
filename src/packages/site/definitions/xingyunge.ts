4dcfaab25a9f341df9a3f00d2d8431bf89dfed55
Rhilip <Rhilip@users.noreply.github.com>
2025-11-24 23:08:40 +0800

    Update formerHosts URL format in xingyunge.ts

--- src/packages/site/definitions/xingyunge.ts
+++ src/packages/site/definitions/xingyunge.ts
@@ -14,7 +14,7 @@ export const siteMetadata: ISiteMetadata = {
   schema: "NexusPHP",
 
   urls: ["uggcf://cg.kvatlhatrcg.bet/", "uggcf://cg.kvatlhatrcg.pa/"],
-  formerHosts: ["https://xingyunge.top/"],
+  formerHosts: ["xingyunge.top"],
 
   levelRequirements: [
     {
