3e9ff6ed1bc2482d0cddeb3caa55ec779c762f81
Rhilip <rhilipruan@gmail.com>
2026-05-28 20:44:31 +0800

    feat(pkg/site): 增加已死亡站点 neu6
    refs: #143

---
+++ src/packages/site/definitions/neu6.ts
@@ -0,0 +1,18 @@
+import type { ISiteMetadata } from "../types";
+
+export const siteMetadata: ISiteMetadata = {
+  version: 1,
+  id: "neu6",
+  name: "六维空间",
+  description: "东北大学ipv6资源分享平台",
+
+  tags: ["教育网", "综合"],
+  collaborator: ["xfl03", "Rhilip"],
+
+  type: "private", // 站点类型，据实修改即可
+  schema: "NexusPHP", // 站点模版，据实修改即可
+
+  urls: ["http://bt.neu6.edu.cn/"],
+
+  isDead: true,
+};
