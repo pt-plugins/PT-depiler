a9421bb7f213edb2e747b9879f821bf8c4968088
ted423 <priston910423@gmail.com>
2026-09-22 16:57:11 +0800

    fix(downloader): Deluge timeout issue
    https://github.com/pt-plugins/PT-depiler/issues/1551

--- src/packages/downloader/entity/Deluge.ts
+++ src/packages/downloader/entity/Deluge.ts
@@ -746,6 +746,7 @@ export default class Deluge extends AbstractBittorrentClient {
       },
       {
         responseType: "json",
+        timeout: this.config.timeout,
       },
     );
     return result;
